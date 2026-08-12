fec01c4bb2de7c26124db2f6539379792f60e286
Priyanshu <132186286+Priyanshu1283@users.noreply.github.com>
2026-08-12 23:26:54 +0530

    Time: 1 ms (99.95%) | Memory: 77.6 MB (14.20%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //Kadanes Algorithm - O(n)
+        int maxSum = Integer.MIN_VALUE;
+        int sum =0;
+        for(int i =0; i< nums.length; i++){
+            sum += nums[i];           
+           maxSum = Math.max(maxSum , sum);
+            if(sum < 0) sum =0;
+        }
+        return maxSum;
+    }
+}
